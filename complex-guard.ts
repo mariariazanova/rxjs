afb364650e67f43b088ff8cb25179e273ac96c51
Maria Riazanova <57406080+mariariazanova@users.noreply.github.com>
2021-11-10 18:19:44 +0300

    Update complex-guard.ts

--- complex-guard.ts
+++ complex-guard.ts
@@ -10,31 +10,31 @@ export class AuthSettingsGuard implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
     return this.httpClient
-      .get(`${this.apiHostUrl}maintenance-service/appconfigurations/${this.applicationBuildName}`, {   //отправляется запрос для получения данных
+      .get(`${this.apiHostUrl}maintenance-service/appconfigurations/${this.applicationBuildName}`, {   //отправляем запрос для получения данных
         headers: { [ignoreAuthorizationHeader]: 'true' },
-      })
-      .pipe(                                                                                           //применяется метод для объединения операторов
-        tap((appSettings: AppSettings) => {                                                            //оператор позволяет осуществить побочные действия, не затрагивая поток
-          this.authSettingsService.authSettings.next(appSettings);                                     //сигнал о том, что поток authSettings испустил новое значение appSettings
+      })                                                                                               //получаем Observable 
+      .pipe(                                                                                           //для дальнейшей работы с потоком данных применяем метод pipe для объединения операторов
+        tap((appSettings: AppSettings) => {                                                            //с помощью оператора tap выполняем побочные действия, не влияя на поток, - 
+          this.authSettingsService.authSettings.next(appSettings);                                     //передаем в поток authSettings новое значение appSettings, полученное посредством отправки запроса в самом начале ?? не уверена!, не понимаю, зачем нам этоу нужно
         }),
-        switchMap(() => {                                                                              //переключается на новый поток, подписывается на него, отписывается от старого
-          const guards = <Token[]>route.data.dependentGuards;                                          //получение зависимых гвардов в зависимости от рута
+        switchMap(() => {                                                                              //переключаемся на новый поток, подписывается на него, отписывается от старого
+          const guards = <Token[]>route.data.dependentGuards;                                          //получаем зависимые (дополнительные?) гварды в зависимости от рута
 
-          return combineLatest(                                                                        //получает последние значения из последовательности при испускании значений
-            guards.map(internalGuard => {
+          return combineLatest(                                                                        //сохраняем последние значения из последовательности ?? не совсем понимаю, зачем он здесь нужен, здесь же один поток? или изменяющийся массив рассматривается как несколько потоков?       
+            guards.map(internalGuard => {                                                              //проходимся по массиву зависимых гвардов
               const guard = this.injector.get(internalGuard);
-              const result = guard.canActivate(route, state);
+              const result = guard.canActivate(route, state);                                          //для каждого гварда получаем его значение 
 
-              if (result instanceof Observable) {
+              if (result instanceof Observable) {                                           
                 return result;
-              } else if (result instanceof Promise) {
+              } else if (result instanceof Promise) {                                                  //если полученное значение не Observable, то преобразуем его в Observable 
                 return from(result);
               } else {
-                return of(result);
+                return of(result);(true
               }
             }),
-          ).pipe(map(params => params.every(value => Boolean(value))));                                 //проходит по массиву значений и заменяет все их параметры на их булевы значения
+          ).pipe(map(params => params.every(value => Boolean(value))));                                 //проходит по массиву значений (каждое из которых массив значений) и заменяет каждый из массивов на его булево значения и получаем что-то похожее на [true, false...]
         }),
       );
   }
-}
+}                                                                                                     //результат должен быть типа of(true) или of(false)? А где мы его получаем, если остается массив of([true, false...])?
